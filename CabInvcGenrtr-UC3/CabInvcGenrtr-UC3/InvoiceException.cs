45222e31e833f62c17d68798358ad6f5360fad57
MSRbuddy <meghanadasari222@gmail.com>
2023-03-01 16:33:15 +0530

    Cab Invoice Generator UseCase-3 is added!

---
+++ CabInvcGenrtr-UC3/CabInvcGenrtr-UC3/InvoiceException.cs
@@ -0,0 +1,17 @@
+﻿namespace CabInvoiceGenerator;
+
+public class InvoiceException : Exception
+{
+    private readonly ExceptionType errorType;
+
+    public enum ExceptionType
+    {
+        NULL_RIDES
+    }
+    public ExceptionType ErrorType { get { return errorType; } }
+
+    public InvoiceException(ExceptionType type, string message) : base(message)
+    {
+        errorType = type;
+    }
+}
---
+++ CabInvcGenrtr-UC3/CabInvcGenrtr-UC3/Program.cs
@@ -0,0 +1,17 @@
+﻿using CabInvoiceGenerator;
+
+namespace CabInvcGenrtr_UC3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome tpo Cab Invoice Generator UC-3!");
+
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            Ride[] rides = { new Ride(2.0, 5), new Ride(1.0, 3) };
+            double TotalFare = invoiceGenerator.MultipleRides(rides);
+            Console.WriteLine($"TotalFare : {TotalFare}");
+        }
+    }
+}
