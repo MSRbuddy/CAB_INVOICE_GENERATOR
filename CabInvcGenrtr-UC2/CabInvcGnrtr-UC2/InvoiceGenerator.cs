caf8a7ad8e4ddaf8b1bb85e45f5d4d59b0d8e900
MSRbuddy <meghanadasari222@gmail.com>
2023-03-01 16:31:08 +0530

    Cab Invoice Generator UseCase-2 is added!

---
+++ CabInvcGenrtr-UC2/CabInvcGnrtr-UC2/InvoiceGenerator.cs
@@ -0,0 +1,36 @@
+﻿namespace CabInvoiceGenerator;
+
+public class InvoiceGenerator
+{
+    private readonly double minFare;
+    private readonly double costPerDistance;
+    private readonly double costPerMinute;
+    public InvoiceGenerator()
+    {
+        minFare = 5;
+        costPerDistance = 10;
+        costPerMinute = 1;
+    }
+
+    public double CalculateFare(double distance, double time)
+    {
+        double totalFare = (distance * costPerDistance) + (time * costPerMinute);
+        return Math.Max(totalFare, minFare);
+    }
+    public double MultipleRides(Ride[] rides)
+    {
+        try
+        {
+            if (rides == null)
+                throw new ArgumentNullException(nameof(rides));
+            double totalFare = 0;
+            foreach (Ride ride in rides)
+                totalFare += CalculateFare(ride.Distance, ride.Time);
+            return totalFare;
+        }
+        catch (ArgumentNullException)
+        {
+            throw new InvoiceException(InvoiceException.ExceptionType.NULL_RIDES, "No Rides was passed in argument");
+        }
+    }
+}
---
+++ CabInvcGenrtr-UC2/CabInvcGnrtr-UC2/Program.cs
@@ -0,0 +1,18 @@
+﻿using CabInvoiceGenerator;
+using System.IO;
+
+namespace CabInvcGnrtr_UC2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Innvoice Generator UC-2!");
+            
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            Ride[] rides = { new Ride(2.0, 5), new Ride(1.0, 3) };
+            double TotalFare = invoiceGenerator.MultipleRides(rides);
+            Console.WriteLine($"TotalFare : {TotalFare}");
+        }
+    }
+}
