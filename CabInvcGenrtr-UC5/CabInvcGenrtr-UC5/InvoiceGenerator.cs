f91a86e0fcd8569b3e75ada8b2a3d9f0711c129e
MSRbuddy <meghanadasari222@gmail.com>
2023-03-01 16:36:18 +0530

    Cab Invoice Generator UseCase-5 is added!

---
+++ CabInvcGenrtr-UC5/CabInvcGenrtr-UC5/InvoiceGenerator.cs
@@ -0,0 +1,43 @@
+﻿namespace CabInvoiceGenerator;
+
+public class InvoiceGenerator
+{
+    private double minFare;
+    private double costPerDistance;
+    private double costPerMinute;
+
+    public double CalculateFare(double distance, double time, bool isPremium = false)
+    {
+        if (isPremium)
+        {
+            minFare = 20;
+            costPerDistance = 15;
+            costPerMinute = 2;
+        }
+        else
+        {
+            minFare = 5;
+            costPerDistance = 10;
+            costPerMinute = 1;
+        }
+        double totalFare = (distance * costPerDistance) + (time * costPerMinute);
+        return Math.Max(totalFare, minFare);
+    }
+
+    public Invoice MultipleRides(Ride[] rides)
+    {
+        try
+        {
+            if (rides == null)
+                throw new ArgumentNullException(nameof(rides));
+            double totalFare = 0;
+            foreach (Ride ride in rides)
+                totalFare += CalculateFare(ride.Distance, ride.Time, ride.IsPremium);
+            return new Invoice(rides.Length, totalFare);
+        }
+        catch (ArgumentNullException)
+        {
+            throw new InvoiceException(InvoiceException.ExceptionType.NULL_RIDES, "No Rides was passed in argument");
+        }
+    }
+}
---
+++ CabInvcGenrtr-UC5/CabInvcGenrtr-UC5/Ride.cs
@@ -0,0 +1,18 @@
+﻿namespace CabInvoiceGenerator;
+
+public class Ride
+{
+    private readonly double distance;
+    private readonly double time;
+    private readonly bool isPremium;
+    public double Distance { get { return distance; } }
+    public double Time { get { return time; } }
+    public bool IsPremium { get { return isPremium; } }
+
+    public Ride(double distance, double time, bool premium = false)
+    {
+        this.distance = distance;
+        this.time = time;
+        this.isPremium = premium;
+    }
+}
