bb5c5930b849d2b534f2429d7533b8d1ca2293c9
MSRbuddy <meghanadasari222@gmail.com>
2023-03-01 16:34:45 +0530

    Cab Invoice Generator UseCase-4 is added!

---
+++ CabInvcGenrtr-UC4/CabInvoiceGenrtr-UC4/InvoiceException.cs
@@ -0,0 +1,19 @@
+﻿namespace CabInvoiceGenerator;
+
+public class InvoiceException : Exception
+{
+    private readonly ExceptionType errorType;
+
+    public enum ExceptionType
+    {
+        NULL_RIDES,
+        INVALID_USER_ID
+    }
+
+    public ExceptionType ErrorType { get { return errorType; } }
+
+    public InvoiceException(ExceptionType type, string message) : base(message)
+    {
+        errorType = type;
+    }
+}
---
+++ CabInvcGenrtr-UC4/CabInvoiceGenrtr-UC4/Program.cs
@@ -0,0 +1,17 @@
+﻿using CabInvoiceGenerator;
+
+namespace CabInvoiceGenrtr_UC4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator UC-4!");
+
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            Ride[] rides = { new Ride(2.0, 5), new Ride(1.0, 3) };
+            double TotalFare = invoiceGenerator.MultipleRides(rides);
+            Console.WriteLine($"TotalFare : {TotalFare}");
+        }
+    }
+}
