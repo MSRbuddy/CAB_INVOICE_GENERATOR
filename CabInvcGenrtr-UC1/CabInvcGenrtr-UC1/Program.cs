483ab432bbc1cc28d74cdae750173f648d373d83
MSRbuddy <meghanadasari222@gmail.com>
2023-03-01 16:28:58 +0530

    Cab Invoice Generator UseCase-1 is added!

---
+++ CabInvcGenrtr-UC1/CabInvcGenrtr-UC1/Program.cs
@@ -0,0 +1,16 @@
+﻿using CabInvoiceGenerator;
+using System.IO;
+
+namespace CabInvcGenrtr_UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator UC-1!");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            double fare = invoiceGenerator.CalculateFare(5.0, 3);
+            Console.WriteLine($"Fare : {fare}");         
+        }
+    }
+}
